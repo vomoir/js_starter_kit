b0092599c86be689d97fa2899e1c4c44147cefa4
Tony Edwards <tedwards@assessor.com.au>
2024-07-17 15:07:31 +1000

    CI-test

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,5 @@
+//  Register babel to transpile before tests run
+require("@babel/register")();
+
+//  Disable webpack features that Mocha doesn't understand
+require.extensions[".css"] = function () {};
